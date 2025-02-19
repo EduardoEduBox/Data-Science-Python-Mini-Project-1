2f85ea853b0f6e8a4445741d5e698e7260905782
EduBox <87114774edu@gmail.com>
2025-02-18 18:02:33 -0800

    To do list part 1 done

---
+++ To-do_list_app_1.py
@@ -0,0 +1,75 @@
+tasks = {}
+
+# adding a task using key, value pairs to make it easy to manage
+def add_task():
+    task_key = input("What is the name of your task? ")
+    if task_key in tasks:
+        print("You already have a task with that name!")
+        return
+
+    task_description = input("And What is its description? ")
+
+    print(f"Task '{task_key}' successfully added!")
+
+    tasks.update({task_key: task_description})
+
+# checking if the item exists, if True, proceed to delete that item from the dict
+def remove_task():
+    if not tasks.keys():
+        print("You don't have any tasks yet...")
+        return
+
+    keys = tasks.keys()
+    formatted_keys = ', '.join(keys)
+    task_remover_key = input(f"""Alright, which task would you like to remove?:
+                (press "c" to cancel) 
+            {formatted_keys}
+            
+    """)
+
+    if task_remover_key == "c":
+        return
+
+    if task_remover_key in tasks:
+        print(f"Task '{task_remover_key}' successfully deleted!")
+        del tasks[task_remover_key]
+    else:
+        print("Please select a valid task. (check for typos or cases)")
+        remove_task()
+
+# simple getter function, retrieving all the values in a user-friendly format
+def view_tasks():
+    if not tasks.keys():
+        print("You don't have any tasks yet...")
+        return
+
+    for key, item in tasks.items():
+        print(f"Task: '{key}' Description: '{item}'")
+
+# Add task when running the app for the first time
+add_task()
+
+# loop that will run this until the person exits the application
+while True:
+    action = int(input("""What would you like to do now?
+                1 - Add another task
+                2 - Remove an existing task
+                3 - View all tasks
+                4 - Exit application
+    """))
+    
+    if action == 1:
+        add_task()
+        continue
+    elif action == 2:
+        remove_task()
+        continue
+    elif action == 3:
+        view_tasks()
+        continue
+    elif action == 4:
+        print("You exited")
+        break
+    else:
+        print("Please enter a valid number!")
+        continue
